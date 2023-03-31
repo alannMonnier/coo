974fe424b822714da169342f20ec025cd1caba57
Alann <alann.monnier-blondeau9@etu.univ-lorraine.fr>
2023-03-31 09:37:47 +0200

    modification fichier.java

--- fichier.java
+++ fichier.java
@@ -1,14 +1,35 @@
+import java.io.*;
+
 public class fichier{
 
     private String nom;
 
+    public fichier(String n){
+        this.nom = n;
+    }
+    
     // chager le fichier
-    public void charger() throws IOException{
-
+    public String charger() throws IOException{
+        BuffferReader br = new BufferReader(new FileReader(this.nom));
+        StringBuilder sb = new StringBuilder();
+        String s = br.readLine();
+        while(s!=null){
+            sb.append(s);
+            s=br.readline();
+        }
+        br.close();
+        return sb.toString();
     }
 
     // sauvegarder le fichier
-    public static void sauver(String nom) throws IOException{
+    public void sauver(String nom) throws IOException{
 
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        fichier f = new fichier("fichier.txt");
+        fichier.sauver("fichier.txt");
+        System.out.println(fichier.charger());
     }
 }
