6e6a9d0142d0db6c4da7a0e27db223b77cc75b1d
Alann <alann.monnier-blondeau9@etu.univ-lorraine.fr>
2023-03-31 09:15:20 +0200

    Implementation fichier.java

---
+++ fichier.java
@@ -0,0 +1,14 @@
+public class fichier{
+
+    private String nom;
+
+    // chager le fichier
+    public void charger() throws IOException{
+
+    }
+
+    // sauvegarder le fichier
+    public static void sauver(String nom) throws IOException{
+
+    }
+}
